1301a877984291602f80dd2e726d8022724b2790
Koraboevvv <bexruzkarabaev@gmail.com>
2025-06-18 23:47:03 +0500

    homework

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const createEl = document.querySelector('.showPopup');
+function showPopup() {
+    createEl.classList.toggle('active');
+}
+const sigInEl = document.querySelector('.showSign');
+function showSign() {
+    sigInEl.classList.toggle('on');
+}
+
+
